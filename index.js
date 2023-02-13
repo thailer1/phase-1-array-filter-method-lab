2b3ba0209fa4c08980a158efb34ff6bd814d54fc
Thailer <thailer.palmer@gmail.com>
2023-02-13 18:53:30 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(function (driver){
+         return driver.toLowerCase() === string.toLowerCase() })
+  }
+  
+  function fuzzyMatch(drivers, string) {
+    return drivers.filter(function (driver){ 
+        return driver.toLowerCase().substring(0, string.length) === string.toLowerCase() })
+  }
+  
+  function matchName(drivers, item) {
+    return drivers.filter(function (driver){
+         return driver.name === item 
+        })
+  }
